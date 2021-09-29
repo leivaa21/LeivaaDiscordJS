8e37623c65286b276dcca8b0c0fddbd02a53ec37
leivaa <adrianleiva212@gmail.com>
2021-09-29 10:06:20 +0200

    You can set WChannel by name or by #it now

--- src/commands/cfg-welcomeChannel.js
+++ src/commands/cfg-welcomeChannel.js
@@ -4,8 +4,12 @@ module.exports = {
     description: `Change the welcome channel for welcome alerts.`,
     async execute(message, args, config, readConfig) {
         if(!args[1] || args[2]!=undefined) return message.channel.send(`Use ${config.prefix}config welcomeChannel {new_channel} to run this command correctly`);
+        
+        var channel;
         const new_channel = args[1];
-        const channel = message.guild.channels.cache.find(ch => ch.name === new_channel);
+
+        if(new_channel[0] == "<") channel = message.guild.channels.cache.find(ch => ch.id === new_channel.slice(2,args[1].length-1));
+        else channel = message.guild.channels.cache.find(ch => ch.name === new_channel); 
         if(channel == undefined) {
             return message.channel.send(`Channel not found, please copy the exact name of the channel!`);
         }
