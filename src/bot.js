3f8e1d659931f0f70695c86bf4239c43dcf54932
leivaa <adrianleiva212@gmail.com>
2021-10-18 12:56:25 +0200

    chore: commenting bot.js and refactorizing a bit

--- src/bot.js
+++ src/bot.js
@@ -7,10 +7,11 @@ import Config from './models/config'
 import loadCommands from './utils/loadCommands'
 
 
-
-
+/************************************************************/
+/* > Setting Up the Bot                                     */
 /**
- * Load configs and display on cmd the actual configs
+ * Load configs and display on cmd when the configs are loaded.
+ * @see class /models/config.js
  */
 new Config(() => {
     console.log("[\x1b[33m LeivaaDiscordJS\x1b[0m ]\n"
@@ -23,103 +24,155 @@ new Config(() => {
 
 /**
  * DiscordBot Instance & login
+ * @see documentation https://discord.js.org/#/docs/main/stable/general/welcome
  */
 const DiscordBot = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"] });
 DiscordBot.login(process.env.discord_token)
 DiscordBot.on('ready', () => {
+    //Displaying on console when the bot is ready to work
     console.log('[\x1b[33m LeivaaDiscordJS\x1b[0m ]' + '\x1b[32m is logged in\x1b[0m');
 });
 
 /**
  * Creating the command collection and reading all 
  * the commands of ./commands.
+ * @see documentation https://discord.js.org/#/docs/main/stable/class/ApplicationCommandManager
+ * @see function /utils/loadCommands.js
  */
 DiscordBot.commands = new Discord.Collection();
 let countCommands = loadCommands(DiscordBot);
+//Displaying on console how much commands the bot has loaded.
 console.log('[\x1b[33m LeivaaDiscordJS\x1b[0m ] ' + '\x1b[33m' + countCommands +'\x1b[0m commands founded and loaded.');
+/************************************************************/
 
+/************************************************************/
+/* > Bot Automated things                                   */
 
 /**
- * Welcome command if someone joins the server
+ * WelcomeMessage (if configured)
+ * @see command /commands/auto-welcome.js
  */
 DiscordBot.on('guildMemberAdd', async(member) =>{
     return DiscordBot.commands.get('welcome').execute(member, Config, Discord);
 })
 
 
-
 /**
- * Controlling the users chat commands
+ * Reaction Role Management (Adding and removing roles)
+ * @see command /commands/auto-reactionRoleAdd
+ * @see command /commands/auto-reactionRoleRemove
  */
+
+DiscordBot.on('messageReactionAdd', async(reaction, user) => {
+    Config.reload();
+    return await DiscordBot.commands.get('reactionRoleAdd').execute(reaction, user, Config.getReactionRole());
+});
+DiscordBot.on('messageReactionRemove', async(reaction, user) => {
+    Config.reload();
+    return await DiscordBot.commands.get('reactionRoleRemove').execute(reaction, user, Config.getReactionRole());
+});
+
+/************************************************************/
+
+/************************************************************/
+/* > Controlling the users chat commands                    */
+
 DiscordBot.on('message', async(message) => {
+    //Displaying on console the author, channel, content and ID of every msg on the guild. 
     console.log(`\x1b[33m${message.author.username}\x1b[0m at\x1b[36m #${message.channel.name} \x1b[0m: ${message.content} \n\x1b[36m| > \x1b[31m MessageID: \x1b[32m ${message.id}\x1b[0m`);
+    
+    //filter commands messages from no command messages
     if (message.content.startsWith(Config.getGlobal().prefix)) {
-
+        //get rid of the prefix and separating the content on an array so it's easier to work with it
         const args = message.content.slice(Config.getGlobal().prefix.length).split(/ +/);
+        //get the command from the args[0] slot (now args[0] is the first word after the command)
         const command = args.shift();
-
+        //always compare the command on lowercase so its not case sensitive
         switch(command.toLowerCase()){
+            /************************************************************/
+            /* > Global Commands, everyone can do it, marked as global- */
 
-            /**
-             * Global Commands (marked as global- on ./commands)
-             */
-
+            //just a command that when someone write "${prefix}ping" the bot will reply with pong!
             case 'ping':
                 message.reply(`🏓 pong!`).then(msg => msg.delete({ timeout: 3 * 1000 }));
                 break;
-
+            /**
+             * Create a simple yes/no poll.
+             * @see command /commands/global-poll 
+             */
             case 'poll':
                 DiscordBot.commands.get('poll').execute(message, args, Config, Discord)
                     .then(msg => {msg ? msg.delete({ timeout: 3 * 1000 }): undefined});
                 break;
-
+            /**
+             * Displays a help embed
+             * @see command /commands/global-help
+             */
             case 'help':
                 DiscordBot.commands.get('help').execute(message, Config, Discord, DiscordBot);
                 break;
+            /************************************************************/
+
+            /************************************************************/
+            /* > Mod Commands, require some permissions, marked as mod- */
             
             /**
-             * Moderation Commands (marked as mod- on ./commands)
+             * kick someone
+             * @see command /commands/mod-kick
              */
             case 'kick':
-
+                //Check if user have permissions to kick
                 if (!message.member.hasPermission(['KICK_MEMBERS'])) {
                     message.reply(`You dont have permissions to \`kick\` someone`)
                         .then(msg => msg.delete({ timeout: 3 * 1000 }));
                     break;
                 }
-
                 DiscordBot.commands.get('kick').execute(message, Config)
                     .then(msg => {msg ? msg.delete({ timeout: 3 * 1000 }): undefined});
                 break;
 
+            /**
+             * ban someone
+             * @see command /commands/mod-ban
+             */
             case 'ban':
-
+                //Check if user have permissions to ban
                 if (!message.member.hasPermission(['BAN_MEMBERS'])) {
                     message.reply(`You dont have permissions to \`ban\` someone`)
                         .then(msg => msg.delete({ timeout: 3 * 1000 }));
                     break;
                 }
-
                 DiscordBot.commands.get('ban').execute(message, args, Config)
                     .then(msg => {msg ? msg.delete({ timeout: 3 * 1000 }): undefined});
                 break;
 
+            /**
+             * Remove n messages on chat
+             * @see command /commands/mod-clear
+             */
             case 'clear':
-
+                //Check if user have permissions to manage messages
                 if (!message.member.hasPermission(['MANAGE_MESSAGES'])) {
                     message.reply(`You dont have permissions to \`clear messages\``)
                         .then(msg => msg.delete({ timeout: 3 * 1000 }));
                     break;
                 }
-
                 DiscordBot.commands.get('clear').execute(message, args, Config)
                     .then(msg => {msg ? msg.delete({ timeout: 3 * 1000 }): undefined});
                 break;
-            
+            /************************************************************/
+
+            /************************************************************/
+            /* > Admin Commands, require admin, marked as adm-          */
+
             /**
-             * Admin Commands (marked as adm- on ./commands)
+             * saves the actual config on a json file
+             * @see class /models/config.js
+             * @see function config::saveConfigs
+             * @see command /commands/adm-saveConfig.js
              */
             case 'saveconfig':
+                //Check if user is admin
                 if (!message.member.hasPermission(['ADMINISTRATOR'])){
                     message.reply(`To run this command you need more power \`(Just for admins :c)\``)
                         .then(msg => msg.delete({ timeout: 3 * 1000 }));
@@ -129,7 +182,14 @@ DiscordBot.on('message', async(message) => {
                     .then(msg => {msg ? msg.delete({ timeout: 3 * 1000 }): undefined});
                 Config.reload();
                 break;
+            /**
+             * loads the saved config json file
+             * @see class /models/config.js
+             * @see function config::loadSaved
+             * @see command /commands/adm-loadConfig.js
+             */
             case 'loadconfig':
+                //Check if user is admin
                 if (!message.member.hasPermission(['ADMINISTRATOR'])){
                     message.reply(`To run this command you need more power \`(Just for admins :c)\``)
                         .then(msg => msg.delete({ timeout: 3 * 1000 }));
@@ -139,24 +199,32 @@ DiscordBot.on('message', async(message) => {
                     .then(msg => {msg ? msg.delete({ timeout: 3 * 1000 }): undefined});
                 Config.reload();
                 break;
+            /**
+             * Send the reaction role embed and active it if its configured
+             * @see command /commands/adm-reactionRole.js
+             */
             case 'reactionrole':
-
+                //Check if user is admin
                 if (!message.member.hasPermission(['ADMINISTRATOR'])){
                     message.reply(`To run this command you need more power \`(Just for admins :c)\``)
                         .then(msg => msg.delete({ timeout: 3 * 1000 }));
                     break;
                 }
-
                 DiscordBot.commands.get('adm-reactionRole').execute(message, Config, Discord)
                     .then(msg => {msg ? msg.delete({ timeout: 3 * 1000 }): undefined});
                 Config.reload();
                 break;
-
+            /**
+             * Here start all the configs commands
+             * @see class config
+             * @see commands /commands/cfg- && /commands/cfgrr-
+             */
             case 'config': 
 
                 /**
                  * All the config command options
                  */
+                //Check if user is admin
                 if (!message.member.hasPermission(['ADMINISTRATOR'])){
                     message.reply(`To run this command you need more power \`(Just for admins :c)\``)
                         .then(msg => msg.delete({ timeout: 3 * 1000 }));
@@ -252,14 +320,3 @@ DiscordBot.on('message', async(message) => {
     }
 });
 
-/**
- * Reaction Role giving and removing roles 
- */
-DiscordBot.on('messageReactionAdd', async(reaction, user) => {
-    Config.reload();
-    return await DiscordBot.commands.get('reactionRoleAdd').execute(reaction, user, Config.getReactionRole());
-});
-DiscordBot.on('messageReactionRemove', async(reaction, user) => {
-    Config.reload();
-    return await DiscordBot.commands.get('reactionRoleRemove').execute(reaction, user, Config.getReactionRole());
-});
